19e842e4f89511cdcc292b68c62651828aa2f367
vivek shekhawat <vshekhawat136@gmail.com>
2026-01-14 21:58:57 +0530

    error removing otp

--- endPoints/otp.js
+++ endPoints/otp.js
@@ -13,7 +13,6 @@ const transporter = nodemailer.createTransport({
   }
 })
 server.put('/', async (req, res) => {
-  console.log('firstError')
   const { email, name } = req.body
   const exists =await ModelLogin.findOne({name,email})
   if (exists) {
@@ -21,7 +20,6 @@ server.put('/', async (req, res) => {
     return res.json({ mess: 'User already exists' })
   }
   const otp = Math.floor(1000 + Math.random() * 9000)
-  console.log('generated')
   try {
     await transporter.sendMail({
       from: process.env.USEREMAIL,
@@ -29,12 +27,9 @@ server.put('/', async (req, res) => {
       subject: 'GigFlow',
       text: `Your OTP is ${otp}`   
     })
-    console.log('transporter')
     await ModelOtp.create({email,otp})
-    console.log('model err')
-    res.json({ mess: 'OTP sent' })
+    return res.json({ mess: 'OTP sent' })
   } catch (err) {
-    console.log(err)
     res.status(500).json({ mess: 'Failed to send OTP' })
   }
 })
