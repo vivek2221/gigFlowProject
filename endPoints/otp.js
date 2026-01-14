f1c83efb8d44624c2bf5e16a7c2db498f8abc045
vivek shekhawat <vshekhawat136@gmail.com>
2026-01-14 21:55:32 +0530

    cahnge edit console.log

--- endPoints/otp.js
+++ endPoints/otp.js
@@ -13,12 +13,15 @@ const transporter = nodemailer.createTransport({
   }
 })
 server.put('/', async (req, res) => {
+  console.log('firstError')
   const { email, name } = req.body
   const exists =await ModelLogin.findOne({name,email})
   if (exists) {
+    console.log('second')
     return res.json({ mess: 'User already exists' })
   }
   const otp = Math.floor(1000 + Math.random() * 9000)
+  console.log('generated')
   try {
     await transporter.sendMail({
       from: process.env.USEREMAIL,
@@ -26,10 +29,12 @@ server.put('/', async (req, res) => {
       subject: 'GigFlow',
       text: `Your OTP is ${otp}`   
     })
+    console.log('transporter')
     await ModelOtp.create({email,otp})
+    console.log('model err')
     res.json({ mess: 'OTP sent' })
   } catch (err) {
-    console.error(err)
+    console.log(err)
     res.status(500).json({ mess: 'Failed to send OTP' })
   }
 })
