563135cf550d6b35d4551f7a9389e4584161fc44
vivek shekhawat <vshekhawat136@gmail.com>
2026-01-14 22:54:33 +0530

    api for otp

--- endPoints/otp.js
+++ endPoints/otp.js
@@ -2,36 +2,24 @@ import express from 'express'
 import nodemailer from 'nodemailer'
 import 'dotenv/config'
 import {ModelLogin, ModelOtp } from '../mongoose/mongooseValidationPlusModelCreation.js'
+import { Resend } from 'resend'
 
-
+const resend = new Resend(process.env.USEREMAIL)
 const server = express.Router()
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-port: 587,
-secure: false, 
-requireTLS: true,
-logger: true,
-debug: true,
-  auth: {
-    user: process.env.USEREMAIL,
-    pass: process.env.PASS 
-  }
-});
 server.put('/', async (req, res) => {
   const { email, name } = req.body
   const exists =await ModelLogin.findOne({name,email})
   if (exists) {
-    console.log('second')
     return res.json({ mess: 'User already exists' })
   }
   const otp = Math.floor(1000 + Math.random() * 9000)
   try {
-    const info=await transporter.sendMail({
-      from: process.env.USEREMAIL,
-      to: email,
-      subject: 'GigFlow',
-      text: `Your OTP is ${otp}`   
-    })
+    resend.emails.send({
+  from: 'onboarding@resend.dev',
+  to: email,
+  subject: 'GigFlow',
+  html: `<p>your otp is<strong>${otp}</strong>!</p>`
+})
     await ModelOtp.create({email,otp})
     return res.json({ mess: 'OTP sent' })
   } catch (err) {
